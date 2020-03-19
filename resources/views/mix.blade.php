e8d4f1ce724f88c09855df55594b69fe99c57d95
Sunel Tr <sunelbe@gmail.com>
2020-03-19 11:04:33 +0530

    updated js dep

--- resources/views/mix.blade.php
+++ resources/views/mix.blade.php
@@ -1,13 +1,13 @@
 @push('styles')
 
-<link href="{{asset(mix('/vendor/eav-dashboard/static/css/18.608d5416.chunk.css', 'vendor/eav-dashboard'))}}" rel="stylesheet" />
+<link href="{{asset(mix('/vendor/eav-dashboard/static/css/18.1a665ece.chunk.css', 'vendor/eav-dashboard'))}}" rel="stylesheet" />
 
 @endpush
 
 @push('scripts')
 
-<script src="{{asset(mix('/vendor/eav-dashboard/static/js/runtime~main.836c7fd6.js', 'vendor/eav-dashboard'))}}" type="text/javascript"></script>
-<script src="{{asset(mix('/vendor/eav-dashboard/static/js/18.58bf8dea.chunk.js', 'vendor/eav-dashboard'))}}" type="text/javascript"></script>
-<script src="{{asset(mix('/vendor/eav-dashboard/static/js/main.77cbb1a6.chunk.js', 'vendor/eav-dashboard'))}}" type="text/javascript"></script>
+<script src="{{asset(mix('/vendor/eav-dashboard/static/js/runtime~main.900956eb.js', 'vendor/eav-dashboard'))}}" type="text/javascript"></script>
+<script src="{{asset(mix('/vendor/eav-dashboard/static/js/18.211bc7e2.chunk.js', 'vendor/eav-dashboard'))}}" type="text/javascript"></script>
+<script src="{{asset(mix('/vendor/eav-dashboard/static/js/main.c942e280.chunk.js', 'vendor/eav-dashboard'))}}" type="text/javascript"></script>
 
 @endpush
